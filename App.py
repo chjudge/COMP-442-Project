a2fc4e978d33d3695c5bd5239b2b7714683a6e54
chjudge <63122915+chjudge@users.noreply.github.com>
2021-11-10 15:49:13 -0500

    register page loads

--- App.py
+++ App.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, url_for, redirect, request, session, f
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, LoginManager, login_required
 from flask_login import login_user, logout_user, current_user
+from flask_bootstrap import Bootstrap
 
 # Make sure this directory is in your Python path for imports
 scriptdir = os.path.dirname(os.path.abspath(__file__))
@@ -11,32 +12,34 @@ sys.path.append(scriptdir)
 from hasher import Hasher
 from forms import RegisterForm, LoginForm
 
-#get database
-dbfile = os.path.join(scriptdir, 'users.sqlite3')
-pepfile = os.path.join(scriptdir, 'pepper.bin')
+dbfile = os.path.join(scriptdir, "users.sqlite3")
+pepfile = os.path.join(scriptdir, "pepper.bin")
 
+# open and read the contents of the pepper file into your pepper key
 with open(pepfile, 'rb') as fin:
   pepper_key = fin.read()
 
 # create a new instance of UpdatedHasher using that pepper key
 pwd_hasher = Hasher(pepper_key)
 
+# Configure the Flask Application
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
-app.config['SECRET_KEY'] = '15vbhwficniuh7h23j8h2892hf92'
+app.config['SECRET_KEY'] = 'correcthorsebatterystaple'
 app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{dbfile}"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(dbfile)
+# Getting the database object handle from the app
+db = SQLAlchemy(app)
+Bootstrap(app)
 
+# Prepare and connect the LoginManager to this app
 app.login_manager = LoginManager()
 app.login_manager.login_view = 'get_login'
 @app.login_manager.user_loader
 def user_loader(id):
     return User.query.get(int(id))
 
-
-#database model
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.Unicode, nullable=False)
@@ -54,7 +57,8 @@ class User(UserMixin, db.Model):
     def verify_password(self, pwd):
         return pwd_hasher.check(pwd, self.password_hash)
 
-db.create_all()
+db.create_all() # this is only needed if the database doesn't already exist
+
 
 @app.get('/register/')
 def get_register():
