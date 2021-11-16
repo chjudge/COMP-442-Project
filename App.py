b1b2cfa706cbfb2e2c4cf5a31f5f1f15e61b04f8
Kevin Hamby <82542818+krhamby@users.noreply.github.com>
2021-11-16 11:08:24 -0500

    Homepage does not work, but will be changed or removed as needed; static folded added; slight modifications to app

--- App.py
+++ App.py
@@ -1,6 +1,7 @@
 from forms import RegisterForm, LoginForm
 from hasher import Hasher
-import os, sys
+import os
+import sys
 from flask import Flask, render_template, url_for, redirect, request, session, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, LoginManager, login_required
@@ -9,7 +10,6 @@ from flask_login import login_user, logout_user, current_user
 # Make sure this directory is in your Python path for imports
 scriptdir = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(scriptdir)
-
 
 dbfile = os.path.join(scriptdir, "users.sqlite3")
 pepfile = os.path.join(scriptdir, "pepper.bin")
@@ -35,11 +35,9 @@ db = SQLAlchemy(app)
 app.login_manager = LoginManager()
 app.login_manager.login_view = 'get_login'
 
-
 @app.login_manager.user_loader
 def user_loader(id):
     return User.query.get(int(id))
-
 
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
@@ -62,7 +60,7 @@ class User(UserMixin, db.Model):
     def verify_password(self, pwd):
         return pwd_hasher.check(pwd, self.password_hash)
 
-#store data for users profile information
+# store data for users profile information
 class UserProfile(db.Model):
     __tablename__ = 'user_profiles'
     profile_id = db.Column(db.Integer, primary_key=True)
@@ -105,11 +103,12 @@ def post_register():
             db.session.commit()
 
             # add name to profile
-            user_profile = UserProfile(user_id=User.query.filter_by(email=r_form.email.data).first().id, fname=r_form.fname.data, lname=r_form.lname.data)
+            user_profile = UserProfile(user_id=User.query.filter_by(
+                email=r_form.email.data).first().id, fname=r_form.fname.data, lname=r_form.lname.data)
             db.session.add(user_profile)
             db.session.commit()
             return redirect(url_for('get_login'))
-        else: # flash error if account exists
+        else:  # flash error if account exists
             print('user exists')
             flash('An account with that email address already exists')
             return redirect(url_for('get_register'))
@@ -138,7 +137,7 @@ def post_login():
             if next is None or not next.startswith('/'):
                 next = url_for('index')
             return redirect(next)
-        else: # flash error if account does not exist
+        else:  # flash error if account does not exist
             print('Invalid email or password')
             flash('Invalid email or password')
             return redirect(url_for('get_login'))
@@ -147,11 +146,6 @@ def post_login():
             print(f"{field}: {error}")
             flash(f"{field}: {error}")
         return redirect(url_for('get_login'))
-
-# return default page
-@app.get('/')
-def index():
-    return render_template('index.html', user=current_user)
 
 # logout user account
 @app.get('/logout/')
@@ -166,7 +160,8 @@ def get_logout():
 @login_required
 def get_profile():
     # pass through the profile of the current user
-    user_profile = UserProfile.query.filter_by(user_id=current_user.get_id()).first()
+    user_profile = UserProfile.query.filter_by(
+        user_id=current_user.get_id()).first()
     return render_template('profile.html', user=current_user, profile=user_profile)
 
 # facilitate updates to user profile information
@@ -176,7 +171,8 @@ def post_profile():
     # update bio
     bio = request.form.get('bio')
     if bio is not None:
-        user_profile = UserProfile.query.filter_by(user_id=current_user.get_id()).first()
+        user_profile = UserProfile.query.filter_by(
+            user_id=current_user.get_id()).first()
         user_profile.bio = bio
         db.session.commit()
     return redirect(url_for('get_profile'))
@@ -203,17 +199,29 @@ def post_admin_page():
             user = User.query.filter_by(id=user_id).first()
             # remove users
             if user is not None:
-                db.session.delete(UserProfile.query.filter_by(user_id=user_id).first())
+                db.session.delete(UserProfile.query.filter_by(
+                    user_id=user_id).first())
                 db.session.delete(user)
                 db.session.commit()
         elif request.form.get('view_profile') is not None:
             user_id = request.form.get('view_profile')
             # get user requested by admin page
             user = User.query.filter_by(id=user_id).first()
-            user_profile = UserProfile.query.filter_by(user_id=user.get_id()).first()
+            user_profile = UserProfile.query.filter_by(
+                user_id=user.get_id()).first()
             # go to user profile
             return render_template('profile.html', user=user, profile=user_profile)
 
         return redirect(url_for('get_admin_page'))
     else:
         return redirect(url_for('index'))
+
+        # return default page
+
+@app.get('/')
+def index():
+    return render_template("homepage.html") # Replaced index.html, user=current_user
+
+@app.get("/about/")
+def get_about_page():
+    pass
