b1bee4bbef3e4ec0597d79e4c3c8c45423dab32c
chjudge <63122915+chjudge@users.noreply.github.com>
2021-11-17 20:27:27 -0500

    added better error messages to all pages

--- forms.py
+++ forms.py
@@ -5,11 +5,11 @@ from wtforms.fields.html5 import EmailField
 from wtforms.validators import InputRequired, Email, EqualTo, Length
 
 class RegisterForm(FlaskForm):
-    email = EmailField("Email: ", validators=[InputRequired(), Email()])
+    email = EmailField("Email: ", validators=[InputRequired(), Email('Email must be in valid format')])
     password = PasswordField("Password: ", 
         validators=[InputRequired(), Length(min=8, max=256)])
     confirm_password = PasswordField("Confirm Password: ", 
-        validators=[EqualTo('password')])
+        validators=[EqualTo('password', message='Passwords must match')])
     submit = SubmitField("Register")
 
 class ProfileForm(FlaskForm):
@@ -20,7 +20,7 @@ class ProfileForm(FlaskForm):
     submit = SubmitField("Update profile")
 
 class LoginForm(FlaskForm):
-    email = EmailField("Email: ", validators=[InputRequired(), Email()])
+    email = EmailField("Email: ", validators=[InputRequired(), Email('Email must be in valid format')])
     password = PasswordField("Password: ", 
         validators=[InputRequired(), Length(min=8, max=256)])
     submit = SubmitField("Login")
